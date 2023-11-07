5fc9cc44f2bddee8d2c2a19b52c29787d4abc25b
Francesco Cagnetta <71755398+fracagnetta@users.noreply.github.com>
2023-11-07 15:05:11 +0100

    Add files via upload
    update with optimised sampling and addition of test notebook

--- datasets/utils.py
+++ datasets/utils.py
@@ -1,11 +1,14 @@
 import torch
 
-def dec2bin(x, bits=None):
+def dec2bin(n, bits=None):
     """
-    Convert decimal number to binary.
-    :param x: decimal number
-    :param bits: number of bits to use. If `None`, the minimum possible is used.
-    :return: x in binary
+    Convert integers to binary.
+    
+    Args:
+            n: The numbers to convert (tensor of size [*]).
+         bits: The length of the representation.
+    Returns:
+        A tensor (size [*, bits]) with the binary representations.
     """
     if bits is None:
         bits = (x.max() + 1).log2().ceil().item()
@@ -13,24 +16,22 @@ def dec2bin(x, bits=None):
     mask = 2 ** torch.arange(bits - 1, -1, -1).to(x.device, x.dtype)
     return x.unsqueeze(-1).bitwise_and(mask).ne(0).float()
 
-def unique(x, dim=-1):
-    """Unique elements of x and indices of those unique elements
-    https://github.com/pytorch/pytorch/issues/36748#issuecomment-619514810
-
-    e.g.
-
-    unique(tensor([
-        [1, 2, 3],
-        [1, 2, 4],
-        [1, 2, 3],
-        [1, 2, 5]
-    ]), dim=0)
-    => (tensor([[1, 2, 3],
-                [1, 2, 4],
-                [1, 2, 5]]),
-        tensor([0, 1, 3]))
+def dec2base(n, b, length=None):
     """
-    unique, inverse = torch.unique(x, return_inverse=True, dim=dim)
-    perm = torch.arange(inverse.size(dim), dtype=inverse.dtype, device=inverse.device)
-    inverse, perm = inverse.flip([dim]), perm.flip([dim])
-    return unique, inverse.new_empty(unique.size(dim)).scatter_(dim, inverse, perm)
+    Convert integers into a different base.
+    
+    Args:
+            n: The numbers to convert (tensor of size [*]).
+            b: The base (integer).
+       length: The length of the representation.
+    Returns:
+        A tensor (size [*, length]) containing the input numbers in the new base.
+    """
+    digits = []
+    while n.sum():
+        digits.append(n % b)
+        n = n.div(b, rounding_mode='floor')
+    if length:
+        assert len(digits) <= length, "Length required is too small to represent input numbers!"
+        digits += [torch.zeros(len(n), dtype=int)] * (length - len(digits))
+    return torch.stack(digits[::-1]).t()
